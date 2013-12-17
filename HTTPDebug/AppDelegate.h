3c41bd7d0e66ff35234c82dd598e46aed67a7fcd
welsonla <wyc.jar@gmail.com>
2013-12-17 13:58:26 +0800

    finish get request

--- HTTPDebug/AppDelegate.h
+++ HTTPDebug/AppDelegate.h
@@ -7,9 +7,32 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AFNetworking.h"
+#import <WebKit/WebKit.h>
+
+typedef enum {
+  GET=0,
+  POST=1
+}PostType;
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *urlField;
+@property (weak) IBOutlet NSPopUpButton *requestTypePop;
+@property (weak) IBOutlet NSButton *runButton;
+
+@property (weak) IBOutlet NSTextField *formIdField;
+@property (weak) IBOutlet NSTextField *actionField;
+@property (unsafe_unretained) IBOutlet NSTextView *responseTextView;
+@property (weak) IBOutlet WebView *responseWebView;
+
+
+
+- (IBAction)startRequest:(id)sender;
+
+- (NSMutableDictionary *)getPOSTParams;
+
+- (void)setResult:(NSString *)response;
 
 @end
--- HTTPDebug/AppDelegate.m
+++ HTTPDebug/AppDelegate.m
@@ -15,4 +15,34 @@
     // Insert code here to initialize your application
 }
 
+- (IBAction)startRequest:(id)sender {
+    NSString *urlString = [self.urlField stringValue];
+    NSURL *url = [NSURL URLWithString:urlString];
+    AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
+    
+    if(self.requestTypePop.selectedTag==POST){
+        NSMutableDictionary *params = [NSMutableDictionary dictionary];
+        
+        [httpClient postPath:[self.actionField stringValue] parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSString *responseString = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+            [self setResult:responseString];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Error:POST");
+        }];
+        
+    }else{
+        [httpClient getPath:urlString parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSString *responseString = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+            [self setResult:responseString];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"ERROR:GET");
+        }];
+    }
+}
+
+
+- (void)setResult:(NSString *)response{
+    [self.responseTextView setString:response];
+    [[self.responseWebView mainFrame] loadHTMLString:response baseURL:nil];
+}
 @end
